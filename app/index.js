3753d4fb2738a6ab37fcb03014cf3ec677ca5a02
Phil Bailey <philbailey@msn.com>
2021-08-10 20:48:46 -0400

    final

--- app/index.js
+++ app/index.js
@@ -1,16 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
-import Popular from "./components/Popular";
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <Popular />
-      </div>
-    );
-  }
-}
+import App from "./components/App";
 
 ReactDom.render(<App />, document.getElementById("app"));
